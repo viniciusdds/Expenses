3b5e1cd76f3061a4372611df10ee2eb4e3ff6d4c
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-06-19 17:37:55 -0300

    Aula 165

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -31,8 +31,6 @@ class _TransactionFormState extends State<TransactionForm> {
     widget.onSubmit(title, value, _selectedDate);
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return  SingleChildScrollView(
